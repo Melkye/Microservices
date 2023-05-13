1f7674ede9faba7fdb01b47740c18708e9bb0776
Melkye <60851956+Melkye@users.noreply.github.com>
2023-05-13 13:17:37 +0300

    change inmemory db to postgres and add migrations

--- BookService/BookService/BookService.Api/Program.cs
+++ BookService/BookService/BookService.Api/Program.cs
@@ -3,23 +3,31 @@ using BookService.DAL.Interfaces;
 using BookService.DAL.Repositories;
 using BookService.BLL.Orchestrators;
 using BookService.DAL.DbAccess;
-using Microsoft.EntityFrameworkCore;
 using BookService.Api.Middlewares;
+using Microsoft.EntityFrameworkCore;
+using BookService.DAL.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<IBookOrchestrator, BookOrchestrator>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 
-builder.Services.AddDbContext<BookDbContext>(options => options.UseInMemoryDatabase("Books"));
+var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+var port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+var db = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var username = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+
+var connectionString = $"host={host};port={port};database={db};username={username};password={password};";
+
+builder.Services.AddNpgsql<BookDbContext>(connectionString);
 
 var app = builder.Build();
 
@@ -30,6 +38,35 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetService<BookDbContext>();
+
+    var pendingMigrations = context.Database.GetPendingMigrations();
+    if (pendingMigrations.Any())
+    {
+        context.Database.Migrate();
+    }
+
+    if (!context.Books.Any())
+    {
+        context.Books.AddRange(
+            new Book()
+            {
+                Id = 1,
+                Title = "HP",
+                Description = "The boy who lived",
+                Author = "JKR"
+            },
+            new Book()
+            {
+                Id = 2,
+                Title = "GoT",
+                Description = "Dragon eggs here",
+                Author = "GRRM"
+            });
+    }
+}
 
 app.UseHttpsRedirection();
 
--- BookService/BookService/BookService.DAL/DbAccess/BookDbContext.cs
+++ BookService/BookService/BookService.DAL/DbAccess/BookDbContext.cs
@@ -1,32 +1,11 @@
 ﻿using BookService.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace BookService.DAL.DbAccess;
 public class BookDbContext : DbContext
 {
     public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
     {
-        Database.EnsureCreated(); // change later to migrations
     }
     public DbSet<Book> Books { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Book>().HasData(
-            new Book()
-            {
-                Id = 1,
-                Title = "HP",
-                Description = "The boy who lived",
-                Author = "JKR"
-            },
-            new Book()
-            {
-                Id = 2,
-                Title = "GoT",
-                Description = "Dragon eggs here",
-                Author = "GRRM"
-            });
-    }
 }
